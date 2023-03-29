cc6af000d03debc2230225f71bab6a9c11141679
Mick Laree <mick.labree@corvid.nl>
2023-03-29 12:49:29 +0200

    alles erg goed in gezet

---
+++ Field.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    public class Field
+    {
+        public int FieldWidth;
+        public int FieldHeight;
+
+        public Field(int width, int height)
+        {
+            this.FieldWidth = width;
+            this.FieldHeight = height;
+        }
+
+        public void Draw()
+        {
+            Console.Clear(); // clear the console before drawing
+
+            // draw the top border
+            for (int i = 0; i < FieldWidth + 2; i++)
+            {
+                Console.Write("█");
+            }
+            Console.WriteLine();
+
+            // draw the sides
+            for (int i = 0; i < FieldHeight; i++)
+            {
+                Console.Write("█");
+                for (int j = 0; j < FieldWidth; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("█");
+            }
+
+            // draw the bottom border
+            for (int i = 0; i < FieldWidth + 2; i++)
+            {
+                Console.Write("█");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize game objects
+            Field field = new Field(40, 20);
+            Snake snake = new Snake(20, 10, 9, 'l');
+            Food food = new Food(field.FieldWidth, field.FieldHeight);
+            int score = 0;
+            
+            // Set up difficulty levels
+            Dictionary<string, int> difficultyLevels = new Dictionary<string, int>();
+            difficultyLevels.Add("Easy", 500);
+            difficultyLevels.Add("Medium", 250);
+            difficultyLevels.Add("Hard", 100);
+
+            // Get user's chosen difficulty level
+            Console.Clear();
+            Console.WriteLine("Choose difficulty level (Easy, Medium, Hard):");
+            string difficulty = Console.ReadLine();
+            int speedMultiplier = difficultyLevels[difficulty];
+
+            // Game loop
+            while (true)
+            {
+                // Draw game objects
+                field.Draw();
+                snake.Draw();
+                food.Draw();
+
+                Console.SetCursorPosition(0, 22);
+                Console.WriteLine("Score: " + score);
+                Console.WriteLine("Press space to pause.");
+                
+                
+                // Handle user input
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            snake.Direction = 'u';
+                            break;
+                        case ConsoleKey.DownArrow:
+                            snake.Direction = 'd';
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            snake.Direction = 'l';
+                            break;
+                        case ConsoleKey.RightArrow:
+                            snake.Direction = 'r';
+                            break;
+                        case ConsoleKey.Spacebar:
+                            snake.Pause();
+                            break;
+                    }
+                }
+
+
+                // Update game objects
+                snake.Move();
+
+                // Check for collisions with food
+                if (snake.Body[0].x == food.XPosition && snake.Body[0].y == food.YPosition)
+                {
+                    snake.Length++;
+                    score++;
+                    food = new Food(field.FieldWidth, field.FieldHeight);
+                }
+
+                // Check for collisions with field edges
+                if (snake.Body[0].x == 0 || snake.Body[0].x == field.FieldWidth + 1 ||
+                    snake.Body[0].y == 0 || snake.Body[0].y == field.FieldHeight + 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Game over! Do you want to play again? (yes/no)");
+                    string gameOverInput = Console.ReadLine();
+
+                    if (gameOverInput.ToLower() == "yes")
+                    {
+                        // Reset game objects and start again
+                        field = new Field(40, 20);
+                        snake = new Snake(20, 10, 3, 'l');
+                        food = new Food(field.FieldWidth, field.FieldHeight);
+                        score = 0;
+                    }
+                    else
+                    {
+                        // Exit the game
+                        return;
+                    }
+                }
+                // Wait for a short time to control the game speed
+                System.Threading.Thread.Sleep(speedMultiplier);
+            }
+        }
+    }
+}
